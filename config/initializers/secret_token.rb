b4033f066cf637db0081d5607341536c5c73a54a
unrealhoang <unrealhoang@gmail.com>
2013-01-23 21:26:43 +0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MfBlog::Application.config.secret_token = '20b8856a7f00d376bed9a75d6cb0b48bc78bc66bfd2d31e494604b44573fc278ba28738c1d321ad07ec5d42c8065f5ee97f6097839b35c5fe557ad43d3d766d2'
