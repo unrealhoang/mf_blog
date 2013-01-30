e8e0642384ee2185b54ffcd7e4f85257e7d2a3b9
witchking <witchking11235@gmail.com>
2013-01-30 17:22:45 +0700

    add functions to re-rate posts if there's a new top article posts paging

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,16 @@ class Post < ActiveRecord::Base
   has_and_belongs_to_many :tags
   belongs_to :author, :class_name => :profile, :foreign_key => :author_id, :validate => true
   belongs_to :category, :validate => true
+
+  def save
+  	Post.update_all(:is_top_article => false) if self.is_top_article
+  	super
+  end
+
+  def update_attributes(params)
+  	Post.update_all(:is_top_article => false) if self.is_top_article
+  	super(params)
+  end
 end
+
+
