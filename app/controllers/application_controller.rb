b632078538121388d9feed9364757d950aaf33b5
unrealhoang <unrealhoang@gmail.com>
2013-02-03 13:23:14 +0700

    add category buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :load_main_categories
+
+  def load_main_categories
+    @main_categories = Category.where(:parent_category_id => nil)
+  end
 end
