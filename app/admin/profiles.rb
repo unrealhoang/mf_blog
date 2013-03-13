d8895de463a55e626836a288fa5cc1aca467bea3
unrealhoang <unrealhoang@gmail.com>
2013-03-13 17:20:58 +0700

    remove url columns in profile show, truncate biography column

--- app/admin/profiles.rb
+++ app/admin/profiles.rb
@@ -14,4 +14,17 @@ ActiveAdmin.register Profile do
     end
     f.actions
   end
+
+  index do 
+    selectable_column
+    column :name
+    column :avatar
+    column :facebook
+    column :linkedin
+    column :twitter
+    column :biography do |profile|
+      truncate(strip_tags(profile.biography).html_safe, :length => 200, :separator => ' ').html_safe
+    end
+    default_actions
+  end
 end
