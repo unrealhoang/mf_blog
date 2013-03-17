476911e62fe15676fdf7eb8cd9ac345de815f433
unrealhoang <unrealhoang@gmail.com>
2013-03-17 15:27:51 +0700

    only show Keywords: when @tags is not empty

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,8 @@ class Tag < ActiveRecord::Base
   attr_accessible :name
 
   has_and_belongs_to_many :posts
+
+  def to_param
+    "#{id}-#{name.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/,'').parameterize}"
+  end
 end
