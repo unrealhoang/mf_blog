279c57ed28a09c722070932336b4360a360fa192
unrealhoang <unrealhoang@gmail.com>
2013-03-19 16:13:25 +0700

    hierachize category list in admin panel, fixed #16

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   attr_accessible :description, :name, :parent_category_id
 
-  has_many :child_category, :class_name => 'Category',
+  has_many :child_categories, :class_name => 'Category',
     :foreign_key => :parent_category_id
   belongs_to :parent_category, :class_name => 'Category'
 
@@ -10,4 +10,17 @@ class Category < ActiveRecord::Base
   def to_param
     "#{id}-#{name.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/,'').parameterize}"
   end
+
+  def self.hierachized_list
+    top_category = where(:parent_category_id => nil).includes(:child_categories).all
+    result = []
+
+    top_category.each do |c|
+      result << c
+      result += c.child_categories.each { |cc| cc.name = "#{c.name} > #{cc.name}" }
+    end
+    
+    return result
+  end
+
 end
